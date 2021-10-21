7ffdfc4f5b5e0789102c223b4504eef7e3ff241a
E15dev <85256998+E15dev@users.noreply.github.com>
2021-10-21 20:13:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 from assets.colorlist import *
 from math import floor
 from assets.testshader import shader
@@ -15,17 +14,20 @@ def pixel(surface, color, pos):
     pygame.draw.line(surface, color, pos, pos)
     return 0
 
+
 def drawshader():
     for i in range(centerwidth - 100, centerwidth + 100):
         for j in range(centerheight - 100, centerheight + 100):
             pixel(screen, shader(i, j, 0), (i, j))
 
+
 def setwindowfps(fps):
     pygame.display.set_caption('The game ' + str(fps) + ' fps')
 
+
 # config
-(width, height) = (500, 500) # screen resolution
-a = pygame.image.load('./assets/icon.png') # old icon, it was supposed to be a game
+(width, height) = (500, 500)  # screen resolution
+a = pygame.image.load('./assets/icon.png')  # old icon, it was supposed to be a game
 
 # init
 centerwidth = floor(width / 2)
@@ -47,9 +49,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    drawshader() # prerendering shader
-    refresh() # swap buffer to screen
-    framestime.append(time.time()) # adding frame time data
+    drawshader()  # prerendering shader
+    refresh()  # swap buffer to screen
+    framestime.append(time.time())  # adding frame time data
     # deleting frame data older than second
     if framestime[0] + 1 < time.time():
         framestime.remove(framestime[0])
