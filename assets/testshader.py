67d4c3221f4050b359a4fb33b20343f87dee3040
E15dev <85256998+E15dev@users.noreply.github.com>
2021-10-21 20:14:29 +0200

    Update testshader.py

--- assets/testshader.py
+++ assets/testshader.py
@@ -1,6 +1,11 @@
-import random
-from math import floor
+import math
+
 
 def shader(x, y, z):
-    # return (255, 255, 255)
-    return (floor((x  + 100) / 2), floor((y  + 100) / 2), 127)
+    color = (dist2d(250, 250, x, y) < 75) * 255  # '255' is center of the screen
+    print(dist2d(0, 0, x, y))
+    return (color, color, color)
+
+
+def dist2d(x0, y0, x1, y1):
+    return math.sqrt(((x0 - x1) ** 2) + ((y0 - y1) ** 2))
